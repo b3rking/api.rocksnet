e46fb04b7d369e2ef3f098d78c06c2910fa671b1
b3rking <ishimweeschylebersy@gmail.com>
2026-06-30 14:41:46 +0200

    added the redis package

--- app/Providers/PrometheusServiceProvider.php
+++ app/Providers/PrometheusServiceProvider.php
@@ -4,14 +4,23 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Prometheus\CollectorRegistry;
-use Prometheus\Storage\InMemory;
+use Prometheus\Storage\Redis;
 
 class PrometheusServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(CollectorRegistry::class, function () {
-            return new CollectorRegistry(new InMemory());
+            Redis::setDefaultOptions([
+                'host' => config('database.redis.default.host', '127.0.0.1'),
+                'port' => config('database.redis.default.port', 6379),
+                'password' => config('database.redis.default.password', null),
+                'timeout' => 0.1,
+                'read_timeout' => 10,
+                'persistent_connections' => false,
+            ]);
+
+            return new CollectorRegistry(new Redis());
         });
     }
 }
